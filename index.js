5e4b081290789e31f91df8911ce18309ffd58767
Julian <julianser@hotmail.com.ar>
2023-07-02 15:48:04 -0300

    Se realizaron las configuraciones Iniciales

---
+++ index.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import cors from 'cors'
+import morgan from 'morgan';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+
+app.set("port", process.env.PORT || 4000)
+app.listen(app.get("port"), () => {
+    console.log("Estoy en el puerto " + app.get("port"));
+});
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended: true}))
+app.use(morgan('dev'));
+
+app.get('/apicolores', (req,res)=>{
+    res.send('Esto es una prueba de la peticion get')
+})
